12dac417cc227bfb6a95d6f30817e62e5efdcb67
ernestoc14 <descreed34@hotmail.com>
2025-01-20 16:58:27 -0500

    Fix: Center the Title in Header Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,9 +45,9 @@ export default function Home() {
         backgroundColor: "#f5f5f5",
       }}
     >
-      <Box sx={{backgroundColor: "#0032a0", width:"100%", display:"flex", paddingX:"50px", paddingY:"30px"}}>
+      <Box sx={{backgroundColor: "#0032a0", width:"100%", display:"flex", gap: "10px", justifyContent: "space-between", paddingX:"50px", paddingY:"30px"}}>
         <Image src="https://www.copaair.com/webassets/images/copa-logo-white.png" alt="Copa Logo" width={200} height={40} />
-        <Typography variant="h4" color="white" sx={{paddingLeft:"400px", fontWeight:"regular"}}>
+        <Typography variant="h4" color="white" sx={{textAlign:"center", flex: 1}}>
           Welcome to Notification Template Builder!!
         </Typography>
       </Box>
@@ -98,58 +98,5 @@ export default function Home() {
         </Box>
       </Box>
     </Box>
-
-    // <div className={styles.page}>
-    //   <main className={styles.main}>
-    //     <h1 className={styles.title}>
-    //       Welcome to Notification Template Builder!!
-    //     </h1>
-    //   </main>
-    //   <section className={styles.section}>
-    //     <div className={styles.stack}>
-    //       <h3>Lista de Componentes</h3>
-    //       <ul className={styles.list}>
-    //         <li>
-    //           <p>Headers</p>
-    //         </ListItem>
-    //         <li>
-    //           <p>Title Box</p>
-    //         </ListItem>
-    //         <li>
-    //           <p>Section Container</p>
-    //         </ListItem>
-    //         <li>
-    //           <p>Buttons</p>
-    //         </ListItem>
-    //         <li>
-    //           <p>Infobar</p>
-    //         </ListItem>
-    //         <li>
-    //           <p>Alerts</p>
-    //         </ListItem>
-    //         <li>
-    //           <p>Banners</p>
-    //         </ListItem>
-    //         <li>
-    //           <p>Itinerary Cards</p>
-    //         </ListItem>
-    //         <li>
-    //           <p>Pax Cards</p>
-    //         </ListItem>
-    //         <li>
-    //           <p>Footer</p>
-    //         </ListItem>
-    //       </ul>
-    //     </div>
-    //     <div className={styles.droppable}>
-    //       <h4>Master Container Droppable Area
-    //       </h4>
-    //       <div className={styles.components}>
-    //         <div dangerouslySetInnerHTML={{__html: PaxCard}} />
-    //         <div dangerouslySetInnerHTML={{__html: TitleBox}} />
-    //       </div>
-    //     </div>
-    //   </section>
-    // </div>
   );
 }
