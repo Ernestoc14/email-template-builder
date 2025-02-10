10d7bfcc17799ddd33e78d361dd47bc09f806b79
ernestoc14 <descreed34@hotmail.com>
2025-02-10 11:11:53 -0500

    Fix: Implementacion de Toggle y Tabs en Home para Language y Device

--- src/app/htmlComponents/Master-container/MasterContainer.tsx
+++ src/app/htmlComponents/Master-container/MasterContainer.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box } from '@mui/material';
 import PropsModal from '@/app/components/PropsModal/PropsModal';
 import DropZone from '@/app/components/DropZone/DropZone';
+import ReactDOM from 'react-dom';
 
 interface MasterContainerProps {
   onDrop?: (component: string, targetId: string) => void;
@@ -11,12 +12,91 @@ const MasterContainer: React.FC<MasterContainerProps> = () => {
   const [componentName, setComponentName] = useState("Headers");
   const [componentVariant, setComponentVariant] = useState("");
   const [propsModalOpen, setPropsModalOpen] = useState(false);
+    // Esta función se dispara cada vez que se hace drop en alguna drop zone
+    const handleDrop = () => {
+      setPropsModalOpen(true);
+    };
+  
 
-  // Esta función se dispara cada vez que se hace drop en alguna drop zone
-  const handleDrop = () => {
-    setPropsModalOpen(true);
-  };
+  const masterContainerHTML = `<div>&nbsp;</div>
+  <table class="email-wrapper" align="center" border="0" cellpadding="0" cellspacing="0" style="padding: 5px 0 0 0;max-width: 800px;">
+    <tr>
+      <td>
+        <table border="0" cellpadding="0" cellspacing="0" style="background-color: #0032A0; border-top-left-radius: 8px; border-top-right-radius: 8px;width: 100%;min-width: 100%;">
+          <tr>
+            <td>
+              <table align="center" border="0" cellpadding="0" cellspacing="0" style="width:100%;min-width:100%;">
+                <tr>
+                  <td class="header-logo-padding" style="padding: 32px 32px 0;">
+                    <div draggable="true"
+                      id="header-logo"
+                      style="border: 1px solid red; min-height: 100px; color: white;"
+                    >
+                      Arrastra aquí el header logo
+                    </div>
+                  </td>
+                </tr>
+              </table>
 
+              <table align="center" border="0" cellpadding="0" cellspacing="0" style="width:100%;min-width:100%;">
+                <tr>
+                  <td class="mobile-padding" style="background-color: #0032A0; padding: 32px 72px">
+                    <div 
+                      id="header-section-bluebg"
+                      style="border: 1px solid red; min-height: 100px; color: white;"
+                    >
+                      Arrastra aquí el header section
+                    </div>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+
+        <table align="center" border="0" cellpadding="0" cellspacing="0" style="width:100%;min-width:100%;">
+          <tr>
+            <td class="mobile-padding" style="padding: 32px;">
+              <div 
+                id="body-section"
+                style="border: 1px solid red; min-height: 100px;"
+              >
+                Arrastra aquí el body section
+              </div>
+            </td>
+          </tr>
+        </table>
+
+        <table align="center" border="0" cellpadding="0" cellspacing="0" style="width:100%;min-width:100%;">
+          <tr>
+            <td class="mobile-padding" style="padding: 32px 56px;background-color:#EFEFEE; border-bottom-left-radius: 8px; border-bottom-right-radius: 8px;">
+              <div id="dropzone-footer"></div>
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>`
+
+    useEffect(() => {
+      setTimeout(() => {
+
+        const container = document.getElementById("dropzone-footer")
+        if (container && !container.hasChildNodes()){
+            ReactDOM.createPortal(  
+              <DropZone
+              id='footer-section'
+              initialContent='Arrastrar Footer'
+              onDrop={() => console.log("Elemento dropeado")}
+              setComponentName={setComponentName}
+              setComponentVariant={setComponentVariant}
+              textColor='black'
+              />,
+              container
+            )
+          }
+        })
+    }, [])
   return (
     <Box>
       <PropsModal 
@@ -25,10 +105,9 @@ const MasterContainer: React.FC<MasterContainerProps> = () => {
         componentName={componentName}
         componentVariant={componentVariant}
       />
+      {/* <Box dangerouslySetInnerHTML={{ __html: masterContainerHTML }} /> */}
       <table className="email-wrapper" align="center" border={0} cellPadding={0} cellSpacing={0} 
-      style={{ padding: '5px 0 0 0', maxWidth: '800px' }}
-      
-      >
+      style={{ padding: '5px 0 0 0', width: '800px', maxWidth: '800px' }}>
         <tbody>
           <tr>
             <td>
@@ -42,7 +121,7 @@ const MasterContainer: React.FC<MasterContainerProps> = () => {
                             <td className="header-logo-padding" style={{ padding: '32px 32px 0' }}>
                               <DropZone
                                 id="header-logo"
-                                initialContent="Arrastra aquí el header logo"
+                                initialContent="Arrastrar Header"
                                 onDrop={handleDrop}
                                 setComponentName={setComponentName}
                                 setComponentVariant={setComponentVariant}
@@ -57,7 +136,7 @@ const MasterContainer: React.FC<MasterContainerProps> = () => {
                             <td className="mobile-padding" style={{ backgroundColor: '#0032A0', padding: '32px 72px' }}>
                               <DropZone
                                 id="header-section-bluebg"
-                                initialContent="Arrastra aquí el header section"
+                                initialContent="Arrastrar Header Section"
                                 onDrop={handleDrop}
                                 setComponentName={setComponentName}
                                 setComponentVariant={setComponentVariant}
@@ -76,7 +155,7 @@ const MasterContainer: React.FC<MasterContainerProps> = () => {
                     <td className="mobile-padding" style={{ padding: '32px' }}>
                       <DropZone
                         id="body-section"
-                        initialContent="Arrastra aquí el body section"
+                        initialContent="Arrastrar Body Section"
                         onDrop={handleDrop}
                         setComponentName={setComponentName}
                         setComponentVariant={setComponentVariant}
@@ -92,7 +171,7 @@ const MasterContainer: React.FC<MasterContainerProps> = () => {
                     <td className="mobile-padding" style={{ padding: '32px 56px', backgroundColor: '#EFEFEE', borderBottomLeftRadius: '8px', borderBottomRightRadius: '8px' }}>
                       <DropZone
                         id="footer-section"
-                        initialContent="Arrastra aquí el footer section"
+                        initialContent="Arrastra Footer"
                         onDrop={handleDrop}
                         setComponentName={setComponentName}
                         setComponentVariant={setComponentVariant}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,17 @@
 import styles from "./page.module.css";
 import { useState } from "react";
 import Image from "next/image";
-import { Box, Button, List, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  List,
+  Tab,
+  Tabs,
+  ToggleButton,
+  ToggleButtonGroup,
+  Typography,
+} from "@mui/material";
 import { ListItemComponent } from "./components/ListItemComponent/ListItemComponent";
-import { MultiButton } from "./components/MultiButton/MultiButton";
 import { WelcomeModal } from "./components/WelcomeModal/WelcomeModal";
 import { CreateNewTemplateModal } from "./components/CreateNewTemplateModal/CreateNewTemplateModal";
 import { HistoryTemplateModal } from "./components/HistoryTemplateModal/HistoryTemplateModal";
@@ -12,36 +20,45 @@ import { ObjectComponents } from "./types/Components";
 import MasterContainer from "./htmlComponents/Master-container/MasterContainer";
 
 export default function Home() {
-
   const [isWelcomeModalOpen, setIsWelcomeModalOpen] = useState(false);
-  const [isCreateNewTemplateModalOpen, setIsCreateNewTemplateModalOpen] = useState(false);
-  const [isHistoryTemplateModalOpen, setIsHistoryTemplateModalOpen] = useState(false);
+  const [isCreateNewTemplateModalOpen, setIsCreateNewTemplateModalOpen] =
+    useState(false);
+  const [isHistoryTemplateModalOpen, setIsHistoryTemplateModalOpen] =
+    useState(false);
+  const [device, setDevice] = useState<string | null>("desktop");
+  const [language, setLanguage] = useState<number>(0);
 
-  // MultiButton Functions
-  const handleLanguageClick = (value: string) => {
-    console.log("Language Clicked: ", value);
-  }
+  // Tabs Functions
+  const handleChangeLanguage = (event: React.SyntheticEvent, newLanguage: number) => {
+    setLanguage(newLanguage)
+  };
 
-  const handleDeviceClick = (value: string) => {
-    console.log("Device Clicked: ", value);
-  }
+  // ToggleButton Functions
+  const handleToggleDevice = (
+    event: React.MouseEvent<HTMLElement>,
+    newDevice: string | null
+  ) => {
+    setDevice(newDevice);
+  };
 
   // WelcomeModal Functions
-  const handleWelcomeModalClose = () => setIsWelcomeModalOpen(false)
-  const handleCreateTemplateModalClose = () => setIsCreateNewTemplateModalOpen(false)
-  const handleHistoryTemplateModalClose = () => setIsHistoryTemplateModalOpen(false)
+  const handleWelcomeModalClose = () => setIsWelcomeModalOpen(false);
+  const handleCreateTemplateModalClose = () =>
+    setIsCreateNewTemplateModalOpen(false);
+  const handleHistoryTemplateModalClose = () =>
+    setIsHistoryTemplateModalOpen(false);
   const handleWelcomeModalClick = (value: string) => {
-    if(value === "Create a New Template") {
-      setIsCreateNewTemplateModalOpen(true)
-    } else if(value === "History Template") {
-      setIsHistoryTemplateModalOpen(true)
+    if (value === "Create a New Template") {
+      setIsCreateNewTemplateModalOpen(true);
+    } else if (value === "History Template") {
+      setIsHistoryTemplateModalOpen(true);
     }
-  }
+  };
   const handleReopenWelcomeModal = () => {
-    setIsCreateNewTemplateModalOpen(false)
-    setIsHistoryTemplateModalOpen(false)
-    setIsWelcomeModalOpen(true)
-  }
+    setIsCreateNewTemplateModalOpen(false);
+    setIsHistoryTemplateModalOpen(false);
+    setIsWelcomeModalOpen(true);
+  };
   return (
     <Box
       sx={{
@@ -52,9 +69,29 @@ export default function Home() {
         backgroundColor: "#f5f5f5",
       }}
     >
-      <Box component="header" sx={{backgroundColor: "#0032a0", width:"100%", display:"flex", gap: "10px", justifyContent: "space-between", paddingX:"50px", paddingY:"30px"}}>
-        <Image src="https://www.copaair.com/webassets/images/copa-logo-white.png" alt="Copa Logo" width={200} height={40} />
-        <Typography variant="h4" color="white" sx={{textAlign:"center", flex: 1}}>
+      <Box
+        component="header"
+        sx={{
+          backgroundColor: "#0032a0",
+          width: "100%",
+          display: "flex",
+          gap: "10px",
+          justifyContent: "space-between",
+          paddingX: "50px",
+          paddingY: "30px",
+        }}
+      >
+        <Image
+          src="https://www.copaair.com/webassets/images/copa-logo-white.png"
+          alt="Copa Logo"
+          width={200}
+          height={40}
+        />
+        <Typography
+          variant="h4"
+          color="white"
+          sx={{ textAlign: "center", flex: 1 }}
+        >
           Notification Template Builder
         </Typography>
       </Box>
@@ -74,13 +111,22 @@ export default function Home() {
             Lista de Componentes
           </Typography>
           <Box className={styles.canva}>
-            <List component="menu" sx={{padding: "0", margin: "0", width:"100%"}}>
-              {Object.keys(ObjectComponents.Components).map((component: string, index: number) => (    
-                <ListItemComponent 
-                  key={index} primary={component} 
-                  isLast={index === Object.keys(ObjectComponents.Components).length - 1} 
+            <List
+              component="menu"
+              sx={{ padding: "0", margin: "0", width: "100%" }}
+            >
+              {Object.keys(ObjectComponents.Components).map(
+                (component: string, index: number) => (
+                  <ListItemComponent
+                    key={index}
+                    primary={component}
+                    isLast={
+                      index ===
+                      Object.keys(ObjectComponents.Components).length - 1
+                    }
                   />
-              ))}
+                )
+              )}
             </List>
           </Box>
         </Box>
@@ -93,30 +139,55 @@ export default function Home() {
               paddingBottom: "20px",
             }}
           >
-            <Typography variant="h6">Droppable Area</Typography>
             <Box sx={{ display: "flex", gap: "60px" }}>
-              <MultiButton buttons={["ES", "EN", "PT", "FR"]} onClick={handleLanguageClick} />
-              <MultiButton buttons={["Desktop", "Mobile"]} onClick={handleDeviceClick}/>
+              <Tabs value={language} onChange={handleChangeLanguage} sx={{ border: "1px solid #e5e5e5"}}>
+                <Tab label="ES" />
+                <Tab label="EN" />
+                <Tab label="PT" />
+                <Tab label="FR" />
+              </Tabs>
+              <ToggleButtonGroup
+                color="primary"
+                exclusive
+                value={device}
+                onChange={handleToggleDevice}
+              >
+                <ToggleButton value="desktop">Desktop</ToggleButton>
+                <ToggleButton value="mobile">Mobile</ToggleButton>
+              </ToggleButtonGroup>
             </Box>
             <Button variant="contained" color="success">
               Generate HTML
             </Button>
           </Box>
           <Box className={styles.canva}>
-            <Box className={styles.components}>Drop a Component inside Master Container!</Box>
-            <Box sx={{
-              width: "100%",
-              display: "flex",
-              justifyContent: "center",
-            }}>
-            <MasterContainer />
+            <Box
+              sx={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <MasterContainer />
             </Box>
           </Box>
         </Box>
       </Box>
-      <WelcomeModal open={isWelcomeModalOpen} onClose={handleWelcomeModalClose} onOptionClick={handleWelcomeModalClick}/>
-      <CreateNewTemplateModal open={isCreateNewTemplateModalOpen} onClose={handleCreateTemplateModalClose} goBack={handleReopenWelcomeModal}/>
-      <HistoryTemplateModal open={isHistoryTemplateModalOpen} onClose={handleHistoryTemplateModalClose} goBack={handleReopenWelcomeModal}/>
+      <WelcomeModal
+        open={isWelcomeModalOpen}
+        onClose={handleWelcomeModalClose}
+        onOptionClick={handleWelcomeModalClick}
+      />
+      <CreateNewTemplateModal
+        open={isCreateNewTemplateModalOpen}
+        onClose={handleCreateTemplateModalClose}
+        goBack={handleReopenWelcomeModal}
+      />
+      <HistoryTemplateModal
+        open={isHistoryTemplateModalOpen}
+        onClose={handleHistoryTemplateModalClose}
+        goBack={handleReopenWelcomeModal}
+      />
     </Box>
   );
 }
